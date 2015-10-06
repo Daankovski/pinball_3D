413b8b43203a5f04aac13093356a97a27c6055c9
vyxia <vyxia@outlook.com>
2015-10-06 11:03:26 +0200

    no message

---
+++ Scripts/ScoreUploader.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreUploader : MonoBehaviour {
+
+
+    //Temporary!
+    bool playerHasDied;
+    private float finalScore;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator sendScore
+    {
+
+    }
+}
