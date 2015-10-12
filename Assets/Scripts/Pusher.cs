3a646aa9fc50027be8fc446cc338dd3cd09b1332
SkaterDanny <dannykruiswijk@hotmail.com>
2015-10-12 09:18:37 +0200

    Added Menu

---
+++ Assets/Scripts/Pusher.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pusher : MonoBehaviour {
+	private Rigidbody _rigidbody;
+	private Vector3 _position;
+	private float x;
+	private float _speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		_rigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		_position = new Vector3 (x,0f,0f);
+		x = Input.GetAxis ("Jump");
+		_rigidbody.AddForce (_position*-_speed);
+	}
+}
