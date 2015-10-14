d30af2822364c364b28ec20328822efeef8c19da
Daankovski <danielepke@hotmail.com>
2015-10-14 15:04:15 +0200

    Commit: Recommited all project files

---
+++ Assets/Scripts/Pusher.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public class Pusher : MonoBehaviour {
+
+	private Rigidbody _rigidbody;
+	private Vector3 _position;
+	private Vector3 initialPosition;
+	[SerializeField]
+	private float _speed = 0f;
+	
+	void Start () {
+		initialPosition = transform.position;
+		_rigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () {
+		_position = new Vector3 (_speed,0f,0f);
+		_rigidbody.AddForce (_position*-_speed*Time.deltaTime);
+		if (Input.GetKey ("space")) {
+			//_speed = 100f;
+			_speed = Random.Range(30,140);
+		}
+		else{
+			_speed = 0f;
+			_rigidbody.position = initialPosition;
+		}
+	}
+}
---
+++ Assets/Scripts/RandomColors.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomColors : MonoBehaviour {
+
+	private Renderer rend;
+	private int randomNumber;
+
+	void Start () {
+		rend = GetComponent<Renderer>();
+	}
+
+	void FixedUpdate () {
+		randomNumber = Random.Range(0,9);
+
+		switch (randomNumber) {
+		case 0:
+			rend.material.color = Color.black;
+			break;
+		case 1:
+			rend.material.color = Color.blue;
+			break;
+		case 2:
+			rend.material.color = Color.cyan;
+			break;
+		case 3:
+			rend.material.color = Color.gray;
+			break;
+		case 4:
+			rend.material.color = Color.green;
+			break;
+		case 5:
+			rend.material.color = Color.magenta;
+			break;
+		case 6:
+			rend.material.color = Color.red;
+			break;
+		case 7:
+			rend.material.color = Color.white;
+			break;
+		case 8:
+			rend.material.color = Color.yellow;
+			break;
+		}
+	}
+}
