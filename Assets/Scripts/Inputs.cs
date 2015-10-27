b4e36c15872a52fa4aecc23d1b1736838627e06f
Daankovski <danielepke@hotmail.com>
2015-10-27 12:10:40 +0100

    Commit: Redone / Re-upload of the Input Script

--- Assets/Scripts/Inputs.cs
+++ Assets/Scripts/Inputs.cs
@@ -1,15 +1,80 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+
 
 public class Inputs : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    [SerializeField]
+    private TestFlipper _flipperL;
+    [SerializeField]
+    private TestFlipper _flipperR;
+    [SerializeField]
+    private Pusher _pusher;
+
+    void Awake() {
+        _flipperL = GetComponent<TestFlipper>();
+        _flipperR = GetComponent<TestFlipper>();
+        _pusher = GetComponentInChildren<Pusher>();
+    }
+
+    void Update () { 
+        keyBoardLayout1();
+        keyBoardLayout2();
+        keyBoardLayout3();
+    }
+
+    void keyBoardLayout1() {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _pusher.FixedUpdate();
+            if (_pusher._speed < 60 || _pusher._speed < 140)
+            {
+                _pusher.isPositive = true;
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+
+           _pusher.isPositive = false;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            print("left arrow was pressed");
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            print("right arrow was pressed");
+        }
+
+    }
+
+    void keyBoardLayout2() {
+        if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            print("numpad 4 is pressed");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Keypad6))
+        {
+            print("numpad 6 is pressed");
+        }
+    }
+
+    void keyBoardLayout3() {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            print("A is pressed");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            print("D is pressed");
+        }
+    }
+
+
 }
--- Assets/Scripts/Pusher.cs
+++ Assets/Scripts/Pusher.cs
@@ -8,24 +8,37 @@ public class Pusher : MonoBehaviour {
 	private Rigidbody _rigidbody;
 	private Vector3 _position;
 	private Vector3 initialPosition;
-	[SerializeField]
-	private float _speed = 0f;
+
+    [HideInInspector] //zorgt er voor dat een public niet in de inspector is te zien.
+    public bool isPositive = false;
+    [HideInInspector] //zorgt er voor dat een public niet in de inspector is te zien.
+	public float _speed = 0f;
+    private int num;
 	
 	void Start () {
 		initialPosition = transform.position;
 		_rigidbody = GetComponent<Rigidbody> ();
 	}
 
-	void FixedUpdate () {
-		_position = new Vector3 (_speed,0f,0f);
-		_rigidbody.AddForce (_position*-_speed*Time.deltaTime);
-		if (Input.GetKey ("space")) {
-			//_speed = 100f;
-			_speed = Random.Range(30,140);
-		}
-		else{
-			_speed = 0f;
-			_rigidbody.position = initialPosition;
-		}
+    public void FixedUpdate () {
+        _position = new Vector3(_speed, 0f, 0f);
+        _rigidbody.AddForce(_position * (_speed * num) * Time.deltaTime);
+
+        switch (isPositive) { 
+            case true:
+                num = 1;
+                _speed = Random.Range(30, 140);
+                break;
+
+            case false:
+                num = -1;
+                _speed = Random.Range(30, 280);
+                break;
+        }
+
+        if (transform.position == initialPosition)
+        {
+            _speed = 0f;
+        }
 	}
 }
